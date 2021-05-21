d8df235de5e7da5e17630eb71c41059c1cc06c97
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-05-22 00:08:00 +0500

    Update JS

--- Project 1/script.js
+++ Project 1/script.js
@@ -25,38 +25,67 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+//function to check if email is valid
+
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())){
+        showSuccess(input)
+    }else{
+        showError(input, `Please provide a valid email`)
+    }
+}
+
+//Function to check if required field have data
+
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+        if(input.value === ''){
+            showError(input, `${getFieldId(input)} is Required`)
+        }else {
+            showSuccess(input)
+        }
+    })
+}
+
+//Function to get the id of the input field
+function getFieldId (input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//Function to check length of input field
+
+function checkLength(input, min, max){
+    if (input.value.length < min){
+        showError(input, `${getFieldId(input)} needs to be atleast ${min} characters`)
+    }else if (input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${max} characters`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+// Function to check if password and confirn password match
+
+function checkPasswordMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2, `Passwords don't match`)
+    }
+}
+
+
+
 
 //Event Listeners
 //Create event listener for submit
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    //check if username is empty
-    if (username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-        username.value = '';
-    }
-    //check if email is empty
-    if (email.value === '') {
-        showError(email, 'Email is required');
-    } else {
-        showSuccess(email);
-        email.value = '';
-    }
-    //check if password is empty
-    if (password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-        password.value = '';
-    }
-    //check if confirm password is empty
-    if (password2.value === '') {
-        showError(password2, 'Confirm Password is required');
-    } else {
-        showSuccess(password2);
-        password2.value = '';
-    }
+
+    checkRequired([username,email,password,password2])
+    checkLength(username, 3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordMatch(password,password2);
 })
