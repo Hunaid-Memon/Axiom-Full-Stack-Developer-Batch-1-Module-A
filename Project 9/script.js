baba4603e44559fbe349a9f9117b9da7c53c859c
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2021-07-04 23:32:05 +0500

    updated P9

--- Project 9/script.js
+++ Project 9/script.js
@@ -1,80 +1,80 @@
-const balance = document.getElementById('balance')
+const balance = document.getElementById('balance');
 const moneyCredit = document.getElementById('money-credit');
 const moneyDebit = document.getElementById('money-debit');
 const list = document.getElementById('list');
+const form = document.getElementById('add-form');
 const reason = document.getElementById('reason');
 const amount = document.getElementById('amount');
-const form = document.getElementById('add-form')
 
 const Transactions = [
     {id: 1, reason: 'Salary', amount: 5000},
-    {id: 2, reason: 'Dinner', amount: -200},
-    {id: 3, reason: 'Utility', amount: -300},
-    {id: 4, reason: 'Expense', amount: -340}
+    {id: 2, reason: 'Dinner', amount: -300},
+    {id: 3, reason: 'Salary', amount: -100},
+    {id: 4, reason: 'Expense', amount: -1000}
 ];
 
 let transactions = Transactions;
 
-function displayTransaction(transaction){
+function updateTransactions(transaction){
     const type = transaction.amount > 0 ? '+' : '-';
-    const transactionLI = document.createElement('li');
-    transactionLI.classList.add(transaction.amount > 0 ? 'credit' : 'debit');
-    transactionLI.innerHTML = `${transaction.reason}<span>${transaction.amount}</span>
-    <button class='dlt-btn' onclick='deleteTransaction(${transaction.id})'>X</button>`
-    list.appendChild(transactionLI)
+    // console.log(transactions.amount, type);
+    let transactionLi = document.createElement('li');
+    transactionLi.classList.add(transaction.amount > 0 ? 'credit' : 'debit');
+    transactionLi.innerHTML = `${transaction.reason}<span>$${transaction.amount}</span>
+    <button class="dlt-btn" onclick='deleteTransaction(${transaction.id})'>X</button>`
+    list.appendChild(transactionLi)
 }
 
 function deleteTransaction(id){
-    transactions = transactions.filter( transaction => transaction.id !== id);
-    init(); 
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    init();
 }
 
-function updateBalance(){
-    const transactionAmounts = transactions.map(transaction => transaction.amount);
-    // console.log(transactionAmounts);
-    const totalBalance = transactionAmounts.reduce((acc, amount) => (acc += amount), 0)
-    // console.log(totalBalance);
-    const creditBalance = transactionAmounts
-                            .filter(amount => amount > 0)
-                            .reduce((acc, account) => (acc += account), 0)
-    const debitBalance = transactionAmounts
-                            .filter(amount => amount < 0)
-                            .reduce((acc,account) => (acc += account), 0)
-    // console.log(debitBalance, creditBalance);
-    balance.innerText = `$${totalBalance}`;
-    moneyCredit.innertext = `$${creditBalance}`;
+function totalBalance(){
+    const transactionAmount = Transactions.map(transaction => transaction.amount);
+    // console.log(transactionAmount);
+    const balanceTotal = transactionAmount.reduce((acc, account) => (acc += account), 0);
+    const creditBalance = transactionAmount
+                                .filter(amount => amount > 0)
+                                .reduce((acc, account) => (acc +=account), 0);
+    const debitBalance = transactionAmount
+                                .filter(amount => amount < 0)
+                                .reduce((acc, account) => (acc += account), 0)
+    
+    balance.innerText = `$${balanceTotal}`;
+    moneyCredit.innerText = `$${creditBalance}`;
     moneyDebit.innerText = `$${debitBalance}`;
-
+    
 }
 
-function addTransacion(e){
-    e.preventDefault();
+function addTransactions(e){
+    e.preventDefault()
     if(reason.value.trim() === '' || amount.value.trim() === ''){
-        alert('Please Provide a valid reason and amount')
-    }else{
+        alert('Please Enter Valid reason and amount')
+    }else {
         const transaction = {
-            id: createID(),
+            id: createId(),
             reason: reason.value,
             amount: +amount.value
         }
         transactions.push(transaction);
-        displayTransaction(transaction);
-        updateBalance();
+        updateTransactions(transaction);
+        totalBalance();
         reason.value = '';
         amount.value = '';
     }
 }
 
-function createID(){
-    return Math.floor(Math.random() * 100000000000)
+function createId(){
+    return Math.floor(Math.random() * 1000000000)
 }
 
-form.addEventListener('submit', addTransacion)
-
-function init(){
-    list.innerHTML = '';
-    transactions.forEach(displayTransaction);
-    updateBalance();
+function init (){
+    list.innerHTML = ''
+    transactions.forEach(updateTransactions);
+    totalBalance()
+    // addTransactions()
 }
+init()
 
-init();
+form.addEventListener('submit', addTransactions)
